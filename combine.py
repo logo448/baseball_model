d5d252746de2f2a1a566ebcfc0bb39da872ea3df
logo448 <stenzellogan@gmail.com>
2015-03-10 20:05:16 -0500

    Added a python file that combines all the csv files into one master file!

---
+++ combine.py
@@ -0,0 +1,22 @@
+import csv
+import os
+
+path = "C:/Users/Logan/Documents/Baseball/2014eve/"
+contents = os.listdir(path)
+master_lst = []
+team_lst = []
+
+for team in contents:
+    if ".csv" in team:
+        with open(path + team, "rb") as f:
+            reader = csv.reader(f)
+            for row in reader:
+                team_lst.append(row)
+            master_lst.append(team_lst)
+            team_lst = []
+
+with open(path + "master.csv", "wb") as f:
+    writer = csv.writer(f)
+    for tmp in master_lst:
+        for data in tmp:
+            writer.writerow(data)
