93050739e7c965aafa0b98c807a2f35c9f28a09d
logo448 <stenzellogan@gmail.com>
2015-03-26 10:18:46 -0500

    Completely overhauled the whole system. Gutted every section that cought a specific event. Added "memory" to the collection system. Keeps track of where the baserunners are!

--- baserunning.py
+++ baserunning.py
@@ -6,131 +6,127 @@ with open("C:/Users/Logan/Documents/Baseball/2014eve/master.csv", "rb") as f:
     reader = csv.reader(f, delimiter=',')
     data = []
     for row in reader:
-        data.append(row[6])
+        data.append((row[2], row[6]))
 
 single = re.compile("\AS[^B]")
 double = re.compile("\AD")
 triple = re.compile("\AT")
-homer = re.compile("\AHR")
 strikeout = re.compile("\AK")
 out = re.compile("\A[0-9]+")
-walk = re.compile("\AW")
 steal2 = re.compile("SB2")
 steal3 = re.compile("SB3")
 advance_info = re.compile("\.")
 multiple_data = re.compile(";")
 
-event_times = {"S": {"1": {"times": 0, "data": []}, "2": {"times": 0, "data": []}, "3": {"times": 0, "data": []}},
+bases = {"1": False, "2": False, "3": False}
+top_bottom = 0
+nxt_top_bottom = 0
+
+event_times = {"S": {"1": {"times": 0, "data": []}, "2": {"times": 0, "data": [], "N": 0}, "3": {"times": 0, "data": [], "N": 0}},
                "D": {"1": {"times": 0, "data": []}, "2": {"times": 0, "data": []}, "3": {"times": 0, "data": []}},
                "T": {"1": {"times": 0, "data": []}, "2": {"times": 0, "data": []}, "3": {"times": 0, "data": []}},
-               "HR": {"1": {"times": 0, "data": []}, "2": {"times": 0, "data": []}, "3": {"times": 0, "data": []}},
                "K": {"total": 0, "1": {"times": 0, "data": []}, "2": {"times": 0, "data": []}, "3": {"times": 0, "data": []}},
                "O": {"total": 0, "1": {"times": 0, "data": []}, "2": {"times": 0, "data": []}, "3": {"times": 0, "data": []}},
-               "W": {"total": 0, "1": {"times": 0, "data": []}, "2": {"times": 0, "data": []}, "3": {"times": 0, "data": []}},
                "S2": {"1": {"times": 0, "data": []}, "2": {"times": 0, "data": []}, "3": {"times": 0, "data": []}},
                "S3": {"1": {"times": 0, "data": []}, "2": {"times": 0, "data": []}, "3": {"times": 0, "data": []}}}
 
-for i in data:
-    if single.search(i) is not None:
-        if advance_info.search(i) is not None:
-            tmp = advance_info.split(i)[1]
-            if multiple_data.search(tmp) is not None:
-                tmp2 = multiple_data.split(tmp)
-                for i2 in tmp2:
-                    if i2[0] != 'B':
-                        event_times["S"][i2[0]]["times"] += 1
-                        if i2[1] != 'X':
-                            event_times["S"][i2[0]]["data"].append(i2[2])
-                        else:
-                            event_times["S"][i2[0]]["data"].append('x'+i2[2])
-            else:
-                if tmp[0] != 'B':
-                    event_times["S"][tmp[0]]["times"] += 1
-                    if tmp[1] != 'X':
-                        event_times["S"][tmp[0]]["data"].append(tmp[2])
-                    else:
-                        event_times["S"][tmp[0]]["data"].append('x'+tmp[2])
+for row in data[:1000]:
+    # check to see if the batting team has switched
+    nxt_top_bottom = row[0]
+    if nxt_top_bottom != top_bottom:
+        top_bottom = nxt_top_bottom
+        # reset bases because of batting team switch
+        bases = {"1": False, "2": False, "3": False}
 
-    if double.search(i) is not None:
-        if advance_info.search(i) is not None:
-            tmp = advance_info.split(i)[1]
-            if multiple_data.search(tmp) is not None:
-                tmp2 = multiple_data.split(tmp)
-                for i2 in tmp2:
-                    if i2[0] != 'B':
-                        event_times["D"][i2[0]]["times"] += 1
-                        if i2[1] != 'X':
-                            event_times["D"][i2[0]]["data"].append(i2[2])
-                        else:
-                            event_times["D"][i2[0]]["data"].append('x'+i2[2])
-            else:
-                if tmp[0] != 'B':
-                    event_times["D"][tmp[0]]["times"] += 1
-                    if tmp[1] != 'X':
-                        event_times["D"][tmp[0]]["data"].append(tmp[2])
-                    else:
-                        event_times["D"][tmp[0]]["data"].append('x'+tmp[2])
+    # catch the data
+    if single.search(row[1]) is not None:
+        # add runner to first base
+        bases["1"] = True
+        # check to see if runner advancement data is present
+        if advance_info.search(row[1]) is not None:
+            # captures the runner advancement data and discards the rest
+            split = advance_info.split(row[1])[1]
+            # check to see if their are multiple runners in the runner advancement data
+            if multiple_data.search(split) is not None:
+                # split data into a list of the runner data
+                split2 = multiple_data.split(split)
 
-    if triple.search(i) is not None:
-        if advance_info.search(i) is not None:
-            tmp = advance_info.split(i)[1]
-            if multiple_data.search(tmp) is not None:
-                tmp2 = multiple_data.split(tmp)
-                for i2 in tmp2:
-                    if i2[0] != 'B':
-                        event_times["T"][i2[0]]["times"] += 1
-                        if i2[1] != 'X':
-                            event_times["T"][i2[0]]["data"].append(i2[2])
-                        else:
-                            event_times["T"][i2[0]]["data"].append('x'+i2[2])
-            else:
-                if tmp[0] != 'B':
-                    event_times["T"][tmp[0]]["times"] += 1
-                    if tmp[1] != 'X':
-                        event_times["T"][tmp[0]]["data"].append(tmp[2])
-                    else:
-                        event_times["T"][tmp[0]]["data"].append('x'+tmp[2])
+                # check to see if some runners didn't advance
+                # init a variable to keep track of the runners in the advancement data
+                recorded_runners = []
+                # loop through each runner in the advancement data
+                for runner in split2:
+                    # add the base where the runner was initially to recorded runners
+                    recorded_runners.append(runner[0])
+                # loop through the bases
+                for base in bases.keys():
+                    # check to see if a runner wasn't mentioned in the advancement data
+                    if bases[base] is True and base not in recorded_runners and base != "1":
+                        # increment variable in data structure representing the number of times a runner was on _ base
+                        event_times["S"][base]["times"] += 1
+                        # record that a runner didn't move
+                        event_times["S"][base]["N"] += 1
 
-    if strikeout.search(i) is not None:
-        if advance_info.search(i) is not None:
-            tmp = advance_info.split(i)[1]
-            if multiple_data.search(tmp) is not None:
-                tmp2 = multiple_data.split(tmp)
-                for i2 in tmp2:
-                    if i2[0] != 'B':
-                        event_times["K"][i2[0]]["times"] += 1
-                        if i2[1] != 'X':
-                            event_times["K"][i2[0]]["data"].append(i2[2])
+                # loop through each runner in the advancement data
+                for runner in split2:
+                    # make sure the runner isn't the batter
+                    if runner[0] != "B":
+                        # increment the variable in the event data structure that keeps track of all the times their
+                        # was a runner on _ base
+                        event_times["S"][runner[0]]["times"] += 1
+                        # set the bool value that correlates to the base the runner was initially on to false
+                        bases[runner[0]] = False
+                        # make sure the runner didn't get out
+                        if runner[1] != "X":
+                            # add the base the runner reached to the event data structure
+                            event_times["S"][runner[0]]["data"].append(runner[2])
+                            # make sure the runner didn't reach home
+                            if runner[2] != "H":
+                                # set the bool value of the base the runner reached to true
+                                bases[runner[2]] = True
+                        # runner got out
                         else:
-                            event_times["K"][i2[0]]["data"].append('x'+i2[2])
+                            # add the out to the data structure
+                            event_times["S"][runner[0]]["data"].append('x'+runner[2])
+            # no data for multiple runners
             else:
-                if tmp[0] != 'B':
-                    event_times["K"][tmp[0]]["times"] += 1
-                    if tmp[1] != 'X':
-                        event_times["K"][tmp[0]]["data"].append(tmp[2])
+                # check to see if runner didn't advance
+                # loop through the bases
+                for base in bases.keys():
+                    # check to see if a runner wasn't mentioned in the advancement data
+                    if bases[base] is True and base != split[0] and base != "1":
+                        # increment variable in data structure representing the number of times a runner was on _ base
+                        event_times["S"][base]["times"] += 1
+                        # record that a runner didn't move
+                        event_times["S"][base]["N"] += 1
+
+                # make sure the runner isn't the batter
+                if split[0] != "B":
+                    # increment the variable in the event data structure that keeps track of all the times their
+                    # was a runner on _ base
+                    event_times["S"][split[0]]["times"] += 1
+                    # set the bool value that correlates to the base the split was initially on to false
+                    bases[split[0]] = False
+                    # make sure the runner didn't get out
+                    if split[1] != "X":
+                        # add the base the runner reached to the event data structure
+                        event_times["S"][split[0]]["data"].append(split[2])
+                        # make sure the runner didn't reach home
+                        if split[2] != "H":
+                            # set the bool value of the base the runner reached to true
+                            bases[split[2]] = True
+                    # runner got out
                     else:
-                        event_times["K"][tmp[0]]["data"].append('x'+tmp[2])
+                        # add the out to the data structure
+                        event_times["S"][split[0]]["data"].append('x' + split[2])
+        # no runner advancement data
         else:
-            event_times["K"]["total"] += 1
-
-    if out.search(i) is not None:
-        if advance_info.search(i) is not None:
-            tmp = advance_info.split(i)[1]
-            if multiple_data.search(tmp) is not None:
-                tmp2 = multiple_data.split(tmp)
-                for i2 in tmp2:
-                    if i2[0] != 'B':
-                        event_times["O"][i2[0]]["times"] += 1
-                        if i2[1] != 'X':
-                            event_times["O"][i2[0]]["data"].append(i2[2])
-                        else:
-                            event_times["O"][i2[0]]["data"].append('x'+i2[2])
-            else:
-                if tmp[0] != 'B':
-                    event_times["O"][tmp[0]]["times"] += 1
-                    if tmp[1] != 'X':
-                        event_times["O"][tmp[0]]["data"].append(tmp[2])
-                    else:
-                        event_times["O"][tmp[0]]["data"].append('x'+tmp[2])
-        else:
-            event_times["O"]["total"] += 1
+            # loop through the bases
+            for base in bases.keys():
+                if bases[base] is True and base != "1":
+                    # increment variable in data structure representing the number of times a runner was on _ base
+                    event_times["S"][base]["times"] += 1
+                    # record that a runner didn't move
+                    event_times["S"][base]["N"] += 1
+print event_times["S"]["3"]
+print len(event_times["S"]["3"]["data"])
